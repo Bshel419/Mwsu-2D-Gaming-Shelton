d6fc6fe228c5048ec37744e86f7f0dfd025b58e0
Bshel419 <Bshel419@rocketmail.com>
2016-06-13 22:17:24 -0400

    Second commit

---
+++ Program_2/Program_2_(I messed up)/assets/load.js
@@ -0,0 +1,32 @@
+var loadState = {
+    preload: function () {
+        // Add a 'loading...' label on the screen
+        var loadingLabel = game.add.text(game.width/2, 150, 'loading...', { font: '30px Arial', fill: '#ffffff' });
+        loadingLabel.anchor.setTo(0.5, 0.5);
+        // Display the progress bar
+        var progressBar = game.add.sprite(game.width/2, 200, 'progressBar');
+        progressBar.anchor.setTo(0.5, 0.5);
+        game.load.setPreloadSprite(progressBar);
+        // Load all our assets
+        game.load.spritesheet('player', 'assets/BERNIE_2.png', 30, 34, 3);
+        game.load.image('enemy', 'assets/HILLARY.png');
+        game.load.image('coin', 'assets/Vote.png');
+        game.load.image('wallV', 'assets/wallVertical.png');
+        game.load.image('wallH', 'assets/wallHorizontal.png');
+        // Load a new asset that we will use in the menu state
+        game.load.image('background', 'assets/background.png');
+		
+		// Sound when the player jumps
+		game.load.audio('jump', ['assets/jump.ogg', 'assets/jump.mp3']);
+		// Sound when the player takes a coin
+		game.load.audio('coin', ['assets/coin.ogg', 'assets/coin.mp3']);
+		// Sound when the player dies
+		game.load.audio('dead', ['assets/Bernie_end.ogg', 'assets/Bernie_end.mp3']);
+		
+		game.load.audio('music', ['assets/Hail to the Chief.ogg', 'assets/Hail to the Chief.mp3']);
+    },
+    create: function() {
+        // Go to the menu state
+        game.state.start('menu');
+    }
+};
