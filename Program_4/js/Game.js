6e11e61dc72585ab5866858fdeeea7f6e70cea76
Bshel419 <Bshel419@rocketmail.com>
2016-07-08 17:23:45 -0400

    Changed the names

---
+++ Program_4/js/Game.js
@@ -0,0 +1,245 @@
+var SpaceHipster = SpaceHipster || {};
+//title screen
+SpaceHipster.Game = function(){};
+
+SpaceHipster.global = {
+		skillLevel : [50, 150],
+		asteriodSize : Math.floor(Math.random() * 100) + 0,
+		bulletTime : 0
+  };
+
+SpaceHipster.Game.prototype = {
+  create: function() {
+  	//set world dimensions
+    this.game.world.setBounds(0, 0, 1920, 1920);
+
+    //background
+    this.background = this.game.add.tileSprite(0, 0, this.game.world.width, this.game.world.height, 'space');
+
+    //create player
+    this.player = this.game.add.sprite(this.game.world.centerX, this.game.world.centerY, 'playership');
+    this.player.scale.setTo(2);
+    this.player.animations.add('fly', [0, 1, 2, 3], 5, true);
+    this.player.animations.play('fly');
+	this.player.anchor.set(0.5);
+	
+	//player bullets
+	this.bullets = this.game.add.group();
+    this.bullets.enableBody = true;
+    this.bullets.physicsBodyType = Phaser.Physics.ARCADE;
+    this.bullets.createMultiple(30, 'bulletImage');
+    this.bullets.setAll('anchor.x', 0.5);
+    this.bullets.setAll('anchor.y', 1);
+    this.bullets.setAll('outOfBoundsKill', true);
+    this.bullets.setAll('checkWorldBounds', true);
+	
+	// Create the emitter with 15 particles. We don't need to set the x y
+	// Since we don't know where to do the explosion yet
+	this.emitter = this.game.add.emitter(0, 0, 15);
+
+	// Set the 'pixel' image for the particles
+	this.emitter.makeParticles('pixel');
+
+    //player initial score of zero
+    this.playerScore = 0;
+
+    //enable player physics
+    this.game.physics.arcade.enable(this.player);
+    this.playerSpeed = 200;
+    this.player.body.collideWorldBounds = true;
+
+    //the camera will follow the player in the world
+    this.game.camera.follow(this.player);
+
+    //generate game elements
+    this.generateCollectables();
+    this.generateAsteriods();
+
+    //show score
+    this.showLabels();
+
+    //sounds
+    this.explosionSound = this.game.add.audio('explosion');
+    console.log(this.explosionSound);
+    this.collectSound = this.game.add.audio('collect');
+	this.asteroidDeathSound = this.game.add.audio('asteroidDeath');
+	
+	 this.game.input.keyboard.addKeyCapture([
+        Phaser.Keyboard.LEFT,
+        Phaser.Keyboard.RIGHT,
+        Phaser.Keyboard.UP,
+        Phaser.Keyboard.DOWN,
+        Phaser.Keyboard.SPACEBAR
+    ]);
+  },
+  update: function() {
+	
+   if (this.game.input.keyboard.isDown(Phaser.Keyboard.UP))
+    {
+        this.game.physics.arcade.accelerationFromRotation(this.player.rotation, 100, this.player.body.acceleration);
+    }
+    else
+    {
+        this.player.body.acceleration.set(0);
+    }
+	
+	if(this.game.input.keyboard.isDown(Phaser.Keyboard.DOWN)){
+		this.game.physics.arcade.accelerationFromRotation(this.player.rotation, -100, this.player.body.acceleration);
+	}
+
+    if (this.game.input.keyboard.isDown(Phaser.Keyboard.LEFT))
+    {
+        this.player.body.angularVelocity = -300;
+    }
+    else if (this.game.input.keyboard.isDown(Phaser.Keyboard.RIGHT))
+    {
+        this.player.body.angularVelocity = 300;
+    }
+    else
+    {
+        this.player.body.angularVelocity = 0;
+    }
+	
+	if(this.game.input.keyboard.isDown(Phaser.Keyboard.SPACEBAR)){
+		this.shootBullet();
+	}
+
+    //collision between player and asteroids
+    this.game.physics.arcade.collide(this.player, this.asteroids, this.hitAsteroid, null, this);
+
+    //overlapping between player and collectables
+    this.game.physics.arcade.overlap(this.player, this.collectables, this.collect, null, this);
+	this.game.physics.arcade.overlap(this.bullets, this.asteroids, this.bulletCollision, null, this);
+  },
+  
+  shootBullet: function() {
+	  //  To avoid them being allowed to fire too fast we set a time limit
+    if (this.game.time.now > SpaceHipster.global.bulletTime)
+    {
+        //  Grab the first bullet we can from the pool
+        bullet = this.bullets.getFirstExists(false);
+
+        if (bullet)
+        {
+			bullet.reset(this.player.x + 16, this.player.y + 16);
+            bullet.lifespan = 2000;
+            bullet.rotation = this.player.rotation;
+            this.game.physics.arcade.velocityFromRotation(this.player.rotation, 400, bullet.body.velocity);
+            SpaceHipster.global.bulletTime = this.game.time.now + 500
+        }
+    }
+  },
+  
+  bulletCollision: function(bullet, asteriod){
+	//  When a bullet hits an asteroid we kill them both
+    bullet.kill();
+    asteriod.kill();
+	
+	this.emitter.x = asteriod.x;
+	this.emitter.y = asteriod.y;
+	this.emitter.start(true, 800, null, 15);
+	this.asteroidDeathSound.play();
+  },
+  
+  generateCollectables: function() {
+    this.collectables = this.game.add.group();
+
+    //enable physics in them
+    this.collectables.enableBody = true;
+    this.collectables.physicsBodyType = Phaser.Physics.ARCADE;
+
+    //phaser's random number generator
+    var numCollectables = this.game.rnd.integerInRange(100, 150)
+    var collectable;
+
+    for (var i = 0; i < numCollectables; i++) {
+      //add sprite
+      collectable = this.collectables.create(this.game.world.randomX, this.game.world.randomY, 'power');
+      collectable.animations.add('fly', [0, 1, 2, 3], 5, true);
+      collectable.animations.play('fly');
+    }
+
+  },
+  generateAsteriods: function() {
+    this.asteroids = this.game.add.group();
+    
+    //enable physics in them
+    this.asteroids.enableBody = true;
+
+    //phaser's random number generator
+    this.numAsteroids = this.game.rnd.integerInRange(SpaceHipster.global.skillLevel[0], SpaceHipster.global.skillLevel[1]);
+	
+	this.generateAsteriod();
+  },
+  
+  generateAsteriod: function() {
+	  var rndmArr = [1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 3, 3, 3, 3, 3, 4, 4, 4, 5, 5];
+	  var asteriod;
+
+    for (var i = 0; i < this.numAsteroids; i++) {
+		
+	var astGen = Phaser.ArrayUtils.getRandomItem(rndmArr);
+      //add sprite
+      asteriod = this.asteroids.create(this.game.world.randomX, this.game.world.randomY, 'rock');
+      asteriod.scale.setTo(astGen);
+	
+		if(astGen > 3){
+		//physics properties
+		asteriod.body.velocity.x = this.game.rnd.pick([-1, 1]) * (astGen * 2);
+		asteriod.body.velocity.y = this.game.rnd.pick([-1, 1]) * (astGen * 2);
+		asteriod.body.immovable = true;
+		asteriod.body.collideWorldBounds = true;
+		}
+		else{
+			asteriod.body.velocity.x = this.game.rnd.pick([-1, 1]) * (astGen * 10);
+			asteriod.body.velocity.y = this.game.rnd.pick([-1, 1]) * (astGen * 10);
+			asteriod.body.immovable = true;
+			asteriod.body.collideWorldBounds = true;
+		}
+    }
+  },
+  hitAsteroid: function(player, asteroid) {
+    //play explosion sound
+    this.explosionSound.play();
+
+    //make the player explode
+    var emitter = this.game.add.emitter(this.player.x, this.player.y, 100);
+    emitter.makeParticles('playerParticle');
+    emitter.minParticleSpeed.setTo(-200, -200);
+    emitter.maxParticleSpeed.setTo(200, 200);
+    emitter.gravity = 0;
+    emitter.start(true, 1000, null, 100);
+    this.player.kill();
+
+    this.game.time.events.add(800, this.gameOver, this);
+  },
+  gameOver: function() {    
+    //pass it the score as a parameter 
+    this.game.state.start('MainMenu', true, false, this.playerScore);
+  },
+  collect: function(player, collectable) {
+    //play collect sound
+    this.collectSound.play();
+
+    //update score
+    this.playerScore++;
+    this.scoreLabel.text = this.playerScore;
+
+    //remove sprite
+    collectable.destroy();
+  },
+  showLabels: function() {
+    //score text
+    var text = "0";
+    var style = { font: "20px Arial", fill: "#fff", align: "center" };
+    this.scoreLabel = this.game.add.text(this.game.width-50, this.game.height - 50, text, style);
+    this.scoreLabel.fixedToCamera = true;
+  }
+};
+
+/*
+TODO
+
+-audio
+-asteriod bounch
+*/
---
+++ Program_7/Program_7/js/play.js
@@ -0,0 +1,292 @@
+var playState = {
+
+    create: function() { 
+
+        this.cursor = game.input.keyboard.createCursorKeys();
+        
+        this.player = game.add.sprite(game.width/2, game.height/2, 'player');
+        this.player.anchor.setTo(0.5, 0.5);
+        game.physics.arcade.enable(this.player);
+        this.player.body.gravity.y = 500;
+		
+		this.player.animations.add('left', [0], 8, true);
+		this.player.animations.add('right', [2], 8, true);
+
+        this.createWorld();
+
+        this.coin = game.add.sprite(60, 140, 'coin');
+        game.physics.arcade.enable(this.coin); 
+        this.coin.anchor.setTo(0.5, 0.5);
+		
+		// Create the emitter with 15 particles. We don't need to set the x y
+		// Since we don't know where to do the explosion yet
+		this.emitter = game.add.emitter(0, 0, 15);
+
+		// Set the 'pixel' image for the particles
+		this.emitter.makeParticles('pixel');
+
+		// Set the x and y speed of the particles between -150 and 150
+		// Speed will be randomly picked between -150 and 150 for each particle
+		this.emitter.setYSpeed(-150, 150);
+		this.emitter.setXSpeed(-150, 150);
+
+		// Scale the particles from 2 time their size to 0 in 800ms
+		// Parameters are: startX, endX, startY, endY, duration
+		this.emitter.setScale(2, 0, 2, 0, 800);
+
+		// Use no gravity
+		this.emitter.gravity = 0;
+
+        this.scoreLabel = game.add.text(30, 30, 'Delegates: 0', { font: '18px Arial', fill: '#ffffff' });
+        game.global.score = 0;
+
+        this.enemies = game.add.group();
+        this.enemies.enableBody = true;
+        this.enemies.createMultiple(10, 'enemy');
+        game.time.events.loop(2200, this.addEnemy, this);
+		
+		//I put 122 so it syncs up with the actual timer display
+		game.time.events.add(Phaser.Timer.SECOND * 122, this.gameEnd, this);
+		this.gameClock = game.add.text(250, 30, 'Seconds Till Convention: 120', { font: '18px Arial', fill: '#ffffff'});
+		
+		this.intTime = 120;
+		game.time.events.loop(Phaser.Timer.SECOND, this.gameTime, this);
+		
+		this.jumpSound = game.add.audio('jump');
+		this.coinSound = game.add.audio('coin');
+		this.deadSound = game.add.audio('dead');
+		this.victorySound = game.add.audio('victory');
+		
+		this.music = game.add.audio('music'); // Add the music
+		this.music.loop = true; // Make it loop
+		this.music.play(); // Start the music
+		
+		if (!game.device.desktop) {
+		this.addMobileInputs();
+		}
+		
+		if (!game.device.dekstop) {
+		// Call 'orientationChange' when the device is rotated
+		game.scale.onOrientationChange.add(this.orientationChange, this);
+
+		// Create an empty label to write the error message if needed
+		this.rotateLabel = game.add.text(game.width/2, game.height/2, '',
+		{ font: '30px Arial', fill: '#fff', backgroundColor: '#000' });
+		this.rotateLabel.anchor.setTo(0.5, 0.5);
+
+		// Call the function at least once
+		this.orientationChange();
+		}
+    },
+	
+	addMobileInputs: function() {
+    // Add the jump button
+    var jumpButton = game.add.sprite(350, 240, 'jumpButton');
+    jumpButton.inputEnabled = true;
+    jumpButton.alpha = 0.5;
+	jumpButton.events.onInputDown.add(this.jumpPlayer, this);
+
+    // Add the move left button
+    var leftButton = game.add.sprite(50, 240, 'leftButton');
+    leftButton.inputEnabled = true;
+    leftButton.alpha = 0.5;
+	leftButton.events.onInputOver.add(this.setLeftTrue, this);
+    leftButton.events.onInputOut.add(this.setLeftFalse, this);
+    leftButton.events.onInputDown.add(this.setLeftTrue, this);
+    leftButton.events.onInputUp.add(this.setLeftFalse, this);
+
+    // Add the move right button
+    var rightButton = game.add.sprite(130, 240, 'rightButton');
+    rightButton.inputEnabled = true;
+    rightButton.alpha = 0.5;
+	rightButton.events.onInputOver.add(this.setRightTrue, this);
+    rightButton.events.onInputOut.add(this.setRightFalse, this);
+    rightButton.events.onInputDown.add(this.setRightTrue, this);
+    rightButton.events.onInputUp.add(this.setRightFalse, this);
+	
+	// Call 'jumpPlayer' when the 'jumpButton' is pressed
+	jumpButton.events.onInputDown.add(this.jumpPlayer, this);
+	},
+	
+	setLeftTrue: function() {
+    this.moveLeft = true;
+	},
+	
+	setLeftFalse: function() {
+    this.moveLeft = false;
+	},
+	
+	setRightTrue: function() {
+    this.moveRight = true;
+	},
+	
+	setRightFalse: function() {
+    this.moveRight = false;
+	},
+	
+	jumpPlayer: function() {
+		// If the player is touching the ground
+		if (this.player.body.onFloor()) {
+			// Jump with sound
+			this.player.body.velocity.y = -320;
+			this.jumpSound.play();
+		}
+	},
+	
+	gameTime: function()
+	{
+		this.gameClock.setText('Seconds Till Convention: ' + this.intTime);
+		this.intTime--;
+	},
+
+    update: function() {
+        // Replaced 'this.walls' by 'this.layer'
+		game.physics.arcade.collide(this.player, this.layer);
+		game.physics.arcade.collide(this.enemies, this.layer);
+        game.physics.arcade.overlap(this.player, this.coin, this.takeCoin, null, this);
+        game.physics.arcade.overlap(this.player, this.enemies, this.playerDie, null, this);
+
+        this.movePlayer(); 
+
+        if (!this.player.inWorld) {
+			this.music.stop();
+			game.state.start('play2');
+        }
+    },
+
+    movePlayer: function() {
+		// If 0 finger are touching the screen
+		if (game.input.totalActivePointers == 0) {
+		// Make sure the player is not moving
+		this.moveLeft = false;
+		this.moveRight = false;
+		}
+		if (this.cursor.left.isDown || game.input.keyboard.isDown(Phaser.Keyboard.A) ||
+		this.moveLeft) { // This is new
+		this.player.body.velocity.x = -200;
+		this.player.animations.play('left');
+		}
+        else if (this.cursor.right.isDown || game.input.keyboard.isDown(Phaser.Keyboard.D) ||
+		this.moveRight) { // This is new
+		this.player.body.velocity.x = 200;
+		this.player.animations.play('right');
+		}
+        else {
+            this.player.body.velocity.x = 0;
+			this.player.frame = 1;
+        }
+		if (this.cursor.up.isDown || game.input.keyboard.isDown(Phaser.Keyboard.W)) {
+        this.jumpPlayer();
+		}	
+    },
+
+    takeCoin: function(player, coin) {
+        game.global.score += 5;
+        this.scoreLabel.text = 'Delegates: ' + game.global.score;
+		
+		// Scale the coin to 0 to make it invisible
+		this.coin.scale.setTo(0, 0);
+		// Grow the coin back to its original scale in 300ms
+		game.add.tween(this.coin.scale).to({x: 1, y: 1}, 300).start();
+		
+		game.add.tween(this.player.scale).to({x: 1.3, y: 1.3}, 100).yoyo(true).start();
+
+		this.coinSound.play();
+
+        this.updateCoinPosition();
+    },
+
+    updateCoinPosition: function() {
+        var coinPosition = [
+            {x: 150, y: 70}, {x: 370, y: 70}, 
+            {x: 70, y: 150}, {x: 450, y: 150}, 
+            {x: 140, y: 310}, {x: 380, y: 310} 
+        ];
+
+        for (var i = 0; i < coinPosition.length; i++) {
+            if (coinPosition[i].x == this.coin.x) {
+                coinPosition.splice(i, 1);
+            }
+        }
+
+        var newPosition = game.rnd.pick(coinPosition);
+        this.coin.reset(newPosition.x, newPosition.y);
+    },
+
+    addEnemy: function() {
+        var enemy = this.enemies.getFirstDead();
+
+        if (!enemy) {
+            return;
+        }
+
+        enemy.anchor.setTo(0.5, 1);
+        enemy.reset(game.width/2, 0);
+        enemy.body.gravity.y = 500;
+        enemy.body.velocity.x = 100 * game.rnd.pick([-1, 1]);
+        enemy.body.bounce.x = 1;
+        enemy.checkWorldBounds = true;
+        enemy.outOfBoundsKill = true;
+    },
+	
+	createWorld: function() {
+		// Create the tilemap
+		this.map = game.add.tilemap('map');
+
+		// Add the tileset to the map
+		this.map.addTilesetImage('tileset');
+
+		// Create the layer by specifying the name of the Tiled layer
+		this.layer = this.map.createLayer('Tile Layer 1');
+
+		// Set the world size to match the size of the layer
+		this.layer.resizeWorld();
+
+		// Enable collisions for the first tilset element (the blue wall)
+		this.map.setCollision(1);
+	},
+	
+	orientationChange: function() {
+		// If the game is in portrait (wrong orientation)
+		if (game.scale.isPortrait) {
+			// Pause the game and add a text explanation
+			game.paused = true;
+			this.rotateLabel.text = 'rotate your device in landscape';
+		}
+		// If the game is in landscape (good orientation)
+		else {
+			// Resume the game and remove the text
+			game.paused = false;
+			this.rotateLabel.text = '';
+		}
+	},
+	
+	startMenu: function() {
+    game.state.start('menu');
+	},
+
+    playerDie: function() {
+		// Kill the player to make it disappear from the screen
+		this.player.kill();
+		// Start the sound and the particles
+		this.deadSound.play();
+		// Set the position of the emitter on top of the player
+		this.emitter.x = this.player.x;
+		this.emitter.y = this.player.y;
+		// Start the emitter by exploding 15 particles that will live 800ms
+		this.emitter.start(true, 800, null, 15);
+		this.music.stop();
+		game.camera.flash(0xffffff, 300);
+		game.camera.shake(0.02, 300);
+		game.time.events.add(1000, this.startMenu, this);
+		
+    },
+	
+	gameEnd: function() {
+		this.music.stop();
+		this.victorySound.play();
+		window.alert("Congratulations! You've beaten Hillary Clinton!")
+		game.state.start('menu');
+		
+	},
+};
